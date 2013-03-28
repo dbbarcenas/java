e1111ec1c606537814dcac85689953674cc39529
durwin <dbbarcenas@gmail.com>
2013-03-28 08:13:05 -0400

    TicTacToe game started with rough plan

---
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -0,0 +1,23 @@
+
+package tictactoe;
+
+/**
+ *
+ * @author durwinbarcenas
+ * 
+ * TicTacToe 
+ * rough plan 
+ * 
+ * 1    Set two dimensional arrays for the user's input. The input Should be either "O" or "X"
+ * 2    Set a two dimensional array for the computer to generate random answer "O" or "X"
+ * 3    Set a method that will compare the user's input and computer's random output
+ * 4    Create a method that will display the output of it all 
+ * 5    
+ */
+public class TicTacToe {
+
+   
+    public static void main(String[] args) {
+       
+    }
+}
