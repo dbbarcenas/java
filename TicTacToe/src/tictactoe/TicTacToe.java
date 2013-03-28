e112915a6dfb55b64477c45d71c85774bf5357a4
durwin <dbbarcenas@gmail.com>
2013-03-28 07:53:46 -0400

    TicTacToe added with rough plam

---
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -0,0 +1,37 @@
+
+package tictactoe;
+
+/**
+ *
+ * @author durwinbarcenas
+ * 
+ * TicTacToe
+ * 
+ * 1    Set two dimensional arrays for the user's input. The input should be either O or X 
+ * 2    Set a two dimensional array for the computer to generate random answer O or X 
+ * 3    Set a method that will compare the user's input and computer's random output 
+ * 4    Create a method that will display the output of it all 
+ * 5    
+ */
+import java.util.Scanner; 
+public class TicTacToe {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public static int getUserInput();
+    {
+        String user_array; 
+            
+            Scanner keyboard = new Scanner(System.in);
+            String [][] userInput = new String [][]; 
+            
+            
+       return user_array; 
+            
+            
+        
+    }
+}
